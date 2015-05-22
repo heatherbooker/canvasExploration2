3674ab3ccd98e2f9d7989c3b9ecc82b86b22bc35
Heather Booker <heather.booker@hotmail.com>
2015-05-22 13:20:23 -0700

    can push elements to the array

--- script.js
+++ script.js
@@ -39,3 +39,37 @@ var makesArray = {
         makesArray.modelArray(numberOfElements);
     },
 };
+
+var positionToPushTo = 0;
+
+var displaysText = {
+    write: function(text, x) {
+        context.fillStyle = "black";
+        context.fillText(text, x, 130);
+    },
+    pushToArray: function() {
+        if (positionToPushTo < 5) {
+            var textToPush = document.getElementById("textInArray").value;
+            if (positionToPushTo === 0) {
+                makesArray.drawBox(100);
+                displaysText.write(textToPush, 120);
+            } else if (positionToPushTo === 1) {
+                makesArray.drawBox(160);
+                displaysText.write(textToPush, 180);
+            } else if (positionToPushTo === 2) {
+                makesArray.drawBox(220);
+                displaysText.write(textToPush, 240);
+            } else if (positionToPushTo === 3) {
+                makesArray.drawBox(280);
+                displaysText.write(textToPush, 300);
+            } else if (positionToPushTo === 4) {
+                makesArray.drawBox(340);
+                displaysText.write(textToPush, 360);
+            };
+            positionToPushTo++;
+            console.log(positionToPushTo);
+        } else {
+            alert('Sorry, this program is only equipped to handle arrays with up to 5 elements.');
+        };
+    },
+}
